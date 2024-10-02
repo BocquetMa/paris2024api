fc8e08dcdbe6d4619f5f96a01b07466133612929
BocquetMa <matheo.bocquet18@gmail.com>
2024-10-02 09:48:50 +0200

    Modification du controller ActualiteController pour ajouter la fonction updateActualite permettant de modifier une actualite

--- src/main/java/bts/sio/api/controller/ActualiteController.java
+++ src/main/java/bts/sio/api/controller/ActualiteController.java
@@ -1,10 +1,13 @@
 package bts.sio.api.controller;
 
 import bts.sio.api.model.Actualite;
+import bts.sio.api.model.Epreuve;
+import bts.sio.api.model.Sport;
 import bts.sio.api.service.ActualiteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 @RestController
@@ -31,6 +34,44 @@ public class ActualiteController {
     @PostMapping("/actualite")
     public Actualite createActualite(@RequestBody Actualite actualite) {
         return actualiteService.saveActualite(actualite);
+    }
+
+    @PutMapping("/actualite/{id}")
+    public Actualite updateActualite(@PathVariable("id") final Long id, @RequestBody Actualite actualite) {
+        Optional<Actualite> a = actualiteService.getActualite(id);
+        if (a.isPresent()) {
+            Actualite currentActualite = a.get();
+
+            String titre = actualite.getTitre();
+            if (titre != null) {
+                currentActualite.setTitre(titre);
+            }
+
+            String contenu = actualite.getContenu();
+            if (contenu != null) {
+                currentActualite.setContenu(contenu);
+            }
+
+            LocalDate dateActualite = actualite.getDateActualite();
+            if (dateActualite != null) {
+                currentActualite.setDateActualite(dateActualite);
+            }
+
+            Sport sport = actualite.getSport();
+            if (sport != null) {
+                currentActualite.setSport(sport);
+            }
+
+            Epreuve epreuve = actualite.getEpreuve();
+            if (epreuve != null) {
+                currentActualite.setEpreuve(epreuve);
+            }
+
+            actualiteService.saveActualite(currentActualite);
+            return currentActualite;
+        } else {
+            return null;
+        }
     }
 
 }
